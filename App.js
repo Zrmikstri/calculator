2f679f71a15bb74950ffff011f48eb6e07f452f8
Zrmikstri <tpvinhhcm@gmail.com>
2022-10-17 21:37:22 +0700

    reformatting App.js

--- App.js
+++ App.js
@@ -18,11 +18,9 @@ function evalEquation(equation) {
 };
 
 const Calculator = () => {
-	const basicNumbers = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
-
+	const basicButtons = ['Del', 'C', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
 	const [expression, setExpression] = useState('');
 	const [result, setResult] = useState('0');
-	const onPress = (value) => setExpression(expression + value);
 
 	return (
 		<View style={styles.container}>
@@ -37,83 +35,18 @@ const Calculator = () => {
 				</Text>
 			</View>
 
-			<View style={styles.col}>
-				<View style={styles.row}>
-					<TouchableOpacity
-						style={styles.button}
-						onPress={() => { setExpression(expression.slice(0, -1)); }}
-					>
-						<Text style={styles.buttonText}>Del</Text>
-					</TouchableOpacity>
-
-					<TouchableOpacity style={styles.button} onPress={() => setExpression('')}>
-						<Text style={styles.buttonText}>Clear</Text>
-					</TouchableOpacity>
-
-					<TouchableOpacity style={styles.button} onPress={() => onPress('/')}>
-						<Text style={styles.buttonText}>/</Text>
-					</TouchableOpacity>
-				</View>
-
-				<View style={styles.row}>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('7')}>
-						<Text style={styles.buttonText}>7</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('8')}>
-						<Text style={styles.buttonText}>8</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('9')}>
-						<Text style={styles.buttonText}>9</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('*')}>
-						<Text style={styles.buttonText}>*</Text>
-					</TouchableOpacity>
-				</View>
-
-				<View style={styles.row}>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('4')}>
-						<Text style={styles.buttonText}>4</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('5')}>
-						<Text style={styles.buttonText}>5</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('6')}>
-						<Text style={styles.buttonText}>6</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('-')}>
-						<Text style={styles.buttonText}>-</Text>
-					</TouchableOpacity>
-				</View>
-
-				<View style={styles.row}>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('1')}>
-						<Text style={styles.buttonText}>1</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('2')}>
-						<Text style={styles.buttonText}>2</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('3')}>
-						<Text style={styles.buttonText}>3</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('+')}>
-						<Text style={styles.buttonText}>+</Text>
-					</TouchableOpacity>
-				</View>
-
-				<View style={styles.row}>
-					<TouchableOpacity style={styles.zeroButton} onPress={() => onPress('0')}>
-						<Text style={{ ...styles.buttonText }}>0</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styles.button} onPress={() => onPress('.')}>
-						<Text style={styles.buttonText}>.</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={styles.button}
-						onPress={() => { setResult(evalEquation(expression)); }}
-					>
-						<Text style={styles.buttonText}>=</Text>
-					</TouchableOpacity>
-				</View>
+			<View style={styles.buttonContainer}>
+				{
+					basicButtons.map((number) => (
+						<TouchableOpacity
+							key={number}
+							style={styles.button}
+							onPress={() => setExpression(expression + number)}
+						>
+							<Text style={styles.buttonText}>{number}</Text>
+						</TouchableOpacity>
+					))
+				}
 			</View>
 		</View >
 	);
@@ -129,6 +62,11 @@ const styles = StyleSheet.create({
 		flex: 1,
 		boderColor: 'black',
 		borderWidth: 2,
+	},
+	buttonContainer: {
+		flex: 4,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
 	},
 	outputText: {
 		flex: 1,
